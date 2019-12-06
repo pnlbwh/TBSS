c6e2e72567d7f51f8642f07b6f4a99585938e966
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-12-06 12:25:07 -0500

    remove ANTs version compatibility check

--- lib/measureSimilarity.py
+++ lib/measureSimilarity.py
@@ -4,26 +4,10 @@ from plumbum import FG
 from multiprocessing import Pool
 import numpy as np
 
-# determine ANTS_VERSION
-# $ antsRegistration --version
-#   ANTs Version: 2.2.0.dev233-g19285
-#   Compiled: Sep  2 2018 23:23:33
-
-antsVerFile='/tmp/ANTS_VERSION_'+environ['USER']
-if not isfile(antsVerFile):
-    (antsRegistration['--version'] > antsVerFile) & FG
-
-with open(antsVerFile) as f:
-      content=f.read().split('\n')
-      ANTS_VERSION= content[0].split()[-1]
 
 def computeMI(target, img, miFile):
 
-    if ANTS_VERSION <= '2.1.0':
-        (MeasureImageSimilarity['3', '2', target, img] | head['-n', '-2'] | cut['-d ', '-f6'] > miFile)()
-
-    else:
-        (MeasureImageSimilarity['-d', '3', '-m', 'MI[{},{},1,256]'.format(target, img)] > miFile) & FG
+    (MeasureImageSimilarity['-d', '3', '-m', 'MI[{},{},1,256]'.format(target, img)] > miFile) & FG
 
 
 def measureSimilarity(imgs, cases, target, logDir, ncpu):
@@ -38,7 +22,8 @@ def measureSimilarity(imgs, cases, target, logDir, ncpu):
     pool.join()
 
     summaryCsv = pjoin(logDir, 'similarity.csv')
-
+    
+    # loop for debugging
     # mis = []
     # with open(summaryCsv, 'w') as fw:
     #     for c in cases:
